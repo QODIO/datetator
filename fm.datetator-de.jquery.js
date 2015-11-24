31d1c3ccf21ec2e78181f9bd0b4dc3b223a37c64
Ingi P. Jacobsen <ingipingi@gmail.com>
2015-11-24 11:06:10 +0000

    German translation (thanks to Roland Hentschel)

---
+++ fm.datetator-de.jquery.js
@@ -0,0 +1,40 @@
+var de_labels = {
+	labels: {
+		week: 'Wo',
+		dayNames: [
+			'Mo',
+			'Di',
+			'Mi',
+			'Do',
+			'Fr',
+			'Sa',
+			'So'
+		],
+		monthNames: [
+			'Januar',
+			'Februar',
+			'März',
+			'April',
+			'Mai',
+			'Juni',
+			'Juli',
+			'August',
+			'September',
+			'Oktober',
+			'November',
+			'Dezember'
+		],
+		previousMonth: '«',
+		nextMonth: '»',
+		previousYear: '«',
+		nextYear: '»',
+		empty: 'Entfernen',
+		today: 'Heute',
+		previousMonthTooltip: 'Zeige vorigen Monat',
+		nextMonthTooltip: 'Zeige nächsten Monat',
+		previousYearTooltip: 'Zeige voriges Jahr',
+		nextYearTooltip: 'Zeige nächstes Jahr',
+		emptyTooltip: 'Entferne Eintrag',
+		todayTooltip: 'Zeige und wähle heutiges Datum'
+	}
+}
